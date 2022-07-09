f3e349832044dff67c27b26c0f8a1f391efe038a
STAung07 <swann1999@gmail.com>
2022-07-10 02:15:04 +0800

    merge with main

--- lib/modules/report/controllers/report_controller.dart
+++ lib/modules/report/controllers/report_controller.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import '/routes/app_pages.dart';
 
 class ReportController extends GetxController {
   final count = 0.obs;
+
+  final TextEditingController textController = TextEditingController();
 
   @override
   void onInit() {
--- lib/modules/report/views/report_view.dart
+++ lib/modules/report/views/report_view.dart
@@ -1,3 +1,4 @@
+import 'package:communiSAFE/modules/report/components/enter_input.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,22 +7,28 @@ import '../controllers/report_controller.dart';
 class ReportView extends GetView<ReportController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(40, 30, 40, 0),
-        child: Container(
-          alignment: Alignment.topLeft,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              const Text('Harlo Monke'),
-              const Text('Fk u Monke'),
-            ],
+    return GetBuilder<ReportController>(
+      builder: (controller) {
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Padding(
+            padding: const EdgeInsets.fromLTRB(40, 30, 40, 0),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  EnterInput(
+                      hint: 'Enter',
+                      controller: controller.textController,
+                      obscureText: false),
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
