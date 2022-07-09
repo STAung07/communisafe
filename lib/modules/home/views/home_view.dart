3ab2070a42713047e0412f0f6236b6667e3562e0
swongts <70316271+swongts@users.noreply.github.com>
2022-07-09 22:28:22 +0800

    Added not working navbar

--- lib/modules/home/views/home_view.dart
+++ lib/modules/home/views/home_view.dart
@@ -23,55 +23,44 @@ class _HomeViewState extends State<HomeView> {
   final controller = Get.find<HomeController>();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('Harlo'),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return GetBuilder<HomeController>(
+      builder: (controller) {
+        return Scaffold(
+          body: SafeArea(
+            child: IndexedStack(
+              index: controller.tabIndex,
+              children: [
+                // HomePage(),
+                // NewsPage(),
+                // AlertsPage(),
+                // AccountPage(),
+              ],
             ),
-            Obx(
-              () => Text(
-                '${controller.count}',
-                style: Theme.of(context).textTheme.headline4,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: controller.changeTabIndex,
+            currentIndex: controller.tabIndex,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
               ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => controller.increment(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+              BottomNavigationBarItem(
+                icon: Icon(Icons.camera),
+                label: 'News',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'Alerts',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Account',
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
