023a94c16b50c1f67d8b174c0de58ed2553fb05c
STAung07 <swann1999@gmail.com>
2022-07-09 18:13:30 +0800

    Flutter Setup

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,41 @@
+part of 'app_pages.dart';
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const SPLASH = _Paths.SPLASH;
+  static const SIGN_OPTIONS = _Paths.SIGN_OPTIONS;
+  static const SIGN_IN = _Paths.SIGN_IN;
+  static const SIGN_UP = _Paths.SIGN_UP;
+  static const FORGOT_PASSWORD = _Paths.FORGOT_PASSWORD;
+  static const WELCOME = _Paths.WELCOME;
+  static const SETUP = _Paths.SETUP;
+  static const BASIC_INFO = _Paths.BASIC_INFO;
+  static const LIFESTYLE = _Paths.LIFESTYLE;
+  static const CONNECT_DEVICE = _Paths.CONNECT_DEVICE;
+  static const CONNECT_SUCCESS = _Paths.CONNECT_SUCCESS;
+  static const CONNECT_FAIL = _Paths.CONNECT_FAIL;
+  static const CONNECT_PROGRESS = _Paths.CONNECT_PROGRESS;
+  static const AGREEMENT = _Paths.AGREEMENT;
+  static const FOOD_LOG = _Paths.FOOD_LOG;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const SPLASH = '/splash';
+  static const SIGN_OPTIONS = '/sign-options';
+  static const SIGN_IN = '/sign-in';
+  static const SIGN_UP = '/sign-up';
+  static const FORGOT_PASSWORD = '/forgot-password';
+  static const WELCOME = '/welcome';
+  static const SETUP = '/setup';
+  static const BASIC_INFO = '/basic-info';
+  static const LIFESTYLE = '/lifestyle';
+  static const CONNECT_DEVICE = '/connect-device';
+  static const CONNECT_SUCCESS = '/connect-success';
+  static const CONNECT_FAIL = '/connect-fail';
+  static const CONNECT_PROGRESS = '/connect-progress';
+  static const AGREEMENT = '/agreement';
+  static const FOOD_LOG = '/food-log';
+}
